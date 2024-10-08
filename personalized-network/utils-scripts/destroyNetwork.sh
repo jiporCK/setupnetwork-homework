be3c8e20b16ef5a78417d4cea88781686b012f87
jiporCK <srengchipor99@gmail.com>
2024-10-08 10:13:11 +0700

    setup personalized network

---
+++ personalized-network/utils-scripts/destroyNetwork.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "..........Destroying network........."
+echo "Removing containers and its volumes ........"
+docker compose down -v 
+just explorer destroy
+
+
+echo "Removing old channel-artifacts artifacts...."
+rm -rf channel-artifacts/*
+echo "Removing old crypto-config artifacts...."
+rm -rf crypto-config/*
